eabf1906f3c2984a9164f9d6db0760a468130a69
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-12-29 19:14:15 +0100

    Setup formatter

--- flake.nix
+++ flake.nix
@@ -1,18 +1,37 @@
 {
   description = "A free and open source 3D creation suite (upstream binaries)";
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
     { self
     , nixpkgs
+    , systems
+    , treefmt-nix
+    , ...
     }:
     let
+      # Small tool to iterate over each systems
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      # Eval the treefmt modules from ./treefmt.nix
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       esp = pkgs.callPackage ./nix { };
     in
     {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      # for `nix fmt`
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      # for `nix flake check`
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+      });
       packages.${system} = {
         inherit (pkgs) esphome;
       };
