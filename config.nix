b05edee8d39882acba4d62898b89951f6b671976
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-12-29 22:55:23 +0100

    Better lambdas

--- config.nix
+++ config.nix
@@ -11,9 +11,14 @@
         turn_off = id: {
           "light.turn_off" = id;
         };
+        get_brightness = id: "id(${id}).current_values.get_brightness()";
       };
       channel = {
         "02" = "chl02";
+      };
+      binary_sensor = {
+        in_01 = "in_01";
+        is_on = id: { "binary_sensor.is_on" = id; };
       };
       switch =
         {
@@ -30,21 +35,42 @@
           {
             condition = conf.cond;
             "then" = conf.then_;
+            "else" = conf.else_ or null;
           };
+      };
+      mkWhile = conf: {
+        while = {
+          condition = conf.cond;
+          "then" = conf.then_;
+        };
       };
       if_on_then_off = id: (mkIf { cond = light.is_on id; then_ = light.turn_off id; });
       if_off_then_on = id: (mkIf { cond = light.is_off id; then_ = light.turn_on id; });
       mkOnClick = action: { "then" = action; };
+      actions = {
+        delay = time: { delay = time; };
+      };
+      lambda = {
+        gt = first: second: { lambda = "return ${first} > ${second}"; };
+        lt = first: second: { lambda = "return ${first} < ${second}"; };
+      };
     in
     {
       dimmer1 =
         let
+          mkVar = id: {
+            inherit id;
+            setUp = { lambda = "id(${id}) = true;"; };
+            setDown = { lambda = "id(${id}) = false;"; };
+            isUp = { lambda = "return id(${id}) == true;"; };
+            isDown = { lambda = "return id(${id}) == false;"; };
+          };
           globals = {
-            lastDirection_W = "lastDirection_W";
-            lastDirection_U = "lastDirection_U";
-            lastDirection_P = "lastDirection_P";
-            lastDirection_T = "lastDirection_T";
-            lastDirection_X = "lastDirection_X";
+            nextDirection_W = mkVar "nextDirection_W";
+            nextDirection_U = mkVar "nextDirection_U";
+            nextDirection_P = mkVar "nextDirection_P";
+            nextDirection_T = mkVar "nextDirection_T";
+            nextDirection_X = mkVar "nextDirection_X";
           };
         in
         {
@@ -149,16 +175,16 @@
               };
             in
             [
-              (mkBoolean globals.lastDirection_W)
-              (mkBoolean globals.lastDirection_U)
-              (mkBoolean globals.lastDirection_P)
-              (mkBoolean globals.lastDirection_T)
-              (mkBoolean globals.lastDirection_X)
+              (mkBoolean globals.nextDirection_W.id)
+              (mkBoolean globals.nextDirection_U.id)
+              (mkBoolean globals.nextDirection_P.id)
+              (mkBoolean globals.nextDirection_T.id)
+              (mkBoolean globals.nextDirection_X.id)
             ];
           binary_sensor = [
             {
               platform = "gpio";
-              id = "in_01";
+              id = binary_sensor.in_01;
               name = "Switch 11.2-TL";
               pin = {
                 pcf8574 = "pcf_inputs";
@@ -173,6 +199,75 @@
                   (if_on_then_off light.u)
                   (if_off_then_on light.u)
                 ];
+
+              on_press = {
+                "then" = [
+                  (mkIf {
+                    cond = {
+                      or = [
+                        (light.is_off light.u)
+                        {
+                          and = [
+                            globals.nextDirection_U.isUp
+                            (lambda.lt (light.get_brightness light.u) "1")
+                          ];
+                        }
+                      ];
+                    };
+                    then_ = [
+                      (actions.delay "0.5s")
+                      {
+                        while = {
+                          condition = binary_sensor.is_on binary_sensor.in_01;
+                          "then" = [
+                            globals.nextDirection_U.setDown
+                            {
+                              "light.dim_relative" = {
+                                id = light.u;
+                                relative_brightness = "5%";
+                                transition_length = "0.1s";
+
+                              };
+                            }
+                            (actions.delay "0.1s")
+                          ];
+                        };
+                      }
+                    ];
+                    "else" = [
+                      (actions.delay "0.5s")
+                      (mkWhile {
+                        cond = {
+                          and = [
+                            (binary_sensor.is_on binary_sensor.in_01)
+                            (light.is_on light.u)
+                            (lambda.gt (light.get_brightness light.u) "0.2")
+                          ];
+                        };
+                        then_ = [
+                          globals.nextDirection_U.setUp
+                          {
+                            "logger.log" =
+                              {
+                                format = "Current brightness %.1f";
+                                args = [ (light.get_brightness light.u) ];
+                              };
+                          }
+                          {
+                            "light.dim_relative" =
+                              {
+                                id = light.u;
+                                relative_brightness = "-5%";
+                                transition_length = "0.1s";
+                              };
+                          }
+                          (actions.delay "0.1s")
+                        ];
+                      })
+                    ];
+                  })
+                ];
+              };
             }
           ];
 
